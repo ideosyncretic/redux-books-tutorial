70b71ceee83537a7d1de34802a69756bb2663724
Sprazzeus <sprazzeus@gmail.com>
2017-05-10 15:03:18 +0800

    Added first action and added it to BookList container

--- src/containers/BookList.js
+++ src/containers/BookList.js
@@ -1,6 +1,9 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux'
+import React, { Component } from 'react'
+import { connect } from 'react-redux' // glue between react and redux
+import { selectBook } from '../actions/index'
+import { bindActionCreators } from 'redux'
 
+// if component should be aware of state, promote it to a container
 class BookList extends Component {
   renderList(){
     return this.props.books.map((book) => {
@@ -17,10 +20,18 @@ class BookList extends Component {
   }
 }
 
+// if state ever changes, this container will auto re-render
 function mapStateToProps(state) {
   return {
     books: state.books
   }
 }
 
-export default connect(mapStateToProps)(BookList)
+// anything returned from this function becomes props on BookList container
+function mapDispatchToProps(dispatch) {
+  // whenever selectBook is called, result should be passed to all reducers
+  return bindActionCreators({ selectBook: selectBook }, dispatch)
+}
+
+// take component, give it some part of state as props, and return a container
+export default connect(mapStateToProps, mapDispatchToProps)(BookList)
