fa16707ba754c97f7e852fdc811a6abc1480740a
Sprazzeus <sprazzeus@gmail.com>
2017-05-21 19:37:32 +0800

    Wrapped up basic lesson

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,7 @@
 export function selectBook(book) {
   // selectBook is an action creator, it needs to return an action object with a type property + payload
   return {
-    type: 'BOOK-SELECTED',
+    type: 'BOOK_SELECTED',
     payload: book
   }
 }
